2a13262b5f3481412fa59bc0e611babc90e94012
abhinav gondkar <abhinavgondkar39@gmail.com>
2022-03-01 19:37:15 +0530

    intial commit

---
+++ src/test/java/com/mh09/com/testGithub/TestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh09.com.testGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
